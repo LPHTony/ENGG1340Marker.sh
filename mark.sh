657831fe0e1ed6076c10da32c11fcdf7914babe5
Foah <expfoahh@outlook.com>
2022-03-02 01:12:30 +0800

    Uploading mark.sh

---
+++ mark.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+Path="./sample_test_cases"
+
+Build_and_Execute () {
+    ProblemNumber="`find ./ -maxdepth 1 -name '*.cpp' -type f -print -quit | grep -Po '\d+'`"
+    if g++ -fdiagnostics-color=always -pedantic-errors -std=c++11 -g ${ProblemNumber}.cpp -o ${ProblemNumber}
+    then
+        for file in `ls ${Path} | grep input`
+        do
+            if test -f ${Path}/${file}
+            then
+                Number="`ls ${Path}/${file} | grep -Po '\d+_\d+'`"
+                ./$ProblemNumber < ${Path}/input${Number}.txt > myoutput${Number}.txt
+                if diff myoutput${Number}.txt ${Path}/output${Number}.txt >/dev/null
+                then
+                    printf "Testcase ${Number} Passed\n"
+                    rm myoutput${Number}.txt
+                else
+                    printf "Testcase ${Number} Failed\n"
+                    diff myoutput${Number}.txt ${Path}/output${Number}.txt
+                fi
+            fi
+        done
+    else
+        echo "g++ Compile failed!"
+    fi
+}
+
+if [ ls ./*.cpp 1>/dev/null 2>&1 == ls ${Path} 1>/dev/null 2>&1 ]
+then
+    Build_and_Execute
+else
+    for dir in `ls`
+    do
+        if test -d $dir
+        then
+            cd ./$dir
+            if ls ./*.cpp 1>/dev/null 2>&1
+            then
+                echo Problem `echo ${dir} | grep -Po '\d+'`:
+                Build_and_Execute
+                echo
+            fi
+            cd ../
+        fi
+    done
+fi
